bd7617167b7e9da589b02a9d04988a44914b22b0
waherof20327 <80187229+waherof20327@users.noreply.github.com>
2022-03-23 23:47:44 +0800

    Create my_datasets.py

---
+++ my_datasets.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+from sklearn.preprocessing import MinMaxScaler
+import csv
+from sklearn.utils import Bunch
+
+def load_my_fancy_dataset():
+    with open(r"C:\\Users\\acer\\Desktop\\測試stata\\006.csv") as csv_file:
+        data_reader = csv.reader(csv_file)
+        feature_names = next(data_reader)[:-1]
+        data = []
+        target = []
+        for row in data_reader:
+            features = row[:-1]
+            label = row[-1]
+            data.append([float(num) for num in features])
+            target.append(int(label))
+        
+        data = np.array(data)
+        target = np.array(target)
+    return Bunch(data=data, target=target, feature_names=feature_names)
